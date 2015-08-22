3aff92c2aafa7fd6a472acb9c47c35aa283e681e
makiable <makiable@gmail.com>
2015-08-22 19:45:38 +0900

    first work
    111

---
+++ Assets/Scrpts/In_GameManager.cs
@@ -0,0 +1,220 @@
+using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class In_GameManager : MonoBehaviour {
+
+	//히어로 컨트롤.
+	public HeroControl mHero01;
+
+	//몬스터 컨트롤 
+	[HideInInspector]
+	public List<MonsterControl> mMonster01;
+
+
+	//텍스트 메세지.
+	public TextMesh mIngTextMassage;
+
+
+	//오토 타겟 몬스터 참조.
+	[HideInInspector]
+	public MonsterControl targetMonster;
+
+	private int monsterSpwanNumber = 3;
+
+	// 몬스터 출연 위치.
+	public Transform[] mSpwanPoint;
+	
+	// 던전을 탐험하는 횟수입니다.
+	private int mLoopCount = 5;
+	
+	// 화면에 나타난 적의 합
+	private int mMonsterCount = 0;
+
+	// 얼마만큼 뛰다가 적을 만날 것인지.
+	private float mRunTime = 1.8f;
+
+
+	public enum StageStatus
+	{
+		Idle,
+		Start,
+		BattleIdle,
+		Battle,
+		Clear,
+	}
+	//기본 스테이지 상황.
+	public StageStatus mStageStatus = StageStatus.Idle;
+
+
+	// Use this for initialization
+	void Start () {
+		// 적 몬스터 들이 담길 List
+		mMonster01 = new List<MonsterControl>();
+		mMonster01.Clear();
+		// 던전 탐험 스텝을 만들어서 순서대로 순환시킵니다.
+		StartCoroutine ("AutoStep");
+
+		Debug.Log ("스타트 코루틴");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+
+	IEnumerator AutoStep()
+	{
+		while (true) 
+		{
+			if (mStageStatus == StageStatus.Idle) 
+			{
+
+				//스타트 TEXT 출현..
+				mIngTextMassage.text = "스타트!";
+				//mTextMesh.text = LiveDays.LiveDaysint.ToString ();
+				Debug.Log("스타트");
+
+				yield return new WaitForSeconds (1.2f);
+
+				mStageStatus = StageStatus.BattleIdle;
+
+			}
+			else if (mStageStatus == StageStatus.BattleIdle) {
+				//몬스터 등장...
+				mHero01.SetStatus(HeroControl.Status.Idle);
+				mMonster01.Clear();
+				for (int i = 0; i < monsterSpwanNumber; i++) {
+					//X 마리의 몬스터를 소환 합니다.
+					SpawnMonster(i);
+
+					//딜레이를 둔다. for 문에 딜레이를 줌.
+					yield return new WaitForSeconds(0.12f);
+				}
+
+				yield return new WaitForSeconds(2); // 2초 대기..
+
+				mIngTextMassage.text="Battle Start";
+
+				//배틀 상태로 둔다..
+				mStageStatus = StageStatus.Battle;
+
+				//코루틴 실행.
+				//StartCoroutine("HeroAutoAttack");
+				//StartCoroutine("MonsterAutoAttack");
+				yield break;
+			}
+		}
+	}
+	
+
+	private void SpawnMonster(int idx)
+	{
+		// Resources 폴더로부터 Monster 프리팹(Prefab)을 로드합니다.
+		Object prefab = Resources.Load("Monster01");
+		
+		// 참조한 프리팹을 인스턴스화 합니다. (화면에 나타납니다.)
+		GameObject monster = Instantiate(prefab, mSpwanPoint[idx].position, Quaternion.identity) as GameObject;
+		monster.transform.parent = mSpwanPoint[idx];
+		
+		Debug.Log ("idx="+idx);
+		
+		
+		// 생성된 인스턴스에서 MonsterControl 컴포넌트를 불러내어 mMonster 리스트에 Add 시킵니다.
+		mMonster01.Add(monster.GetComponent<MonsterControl>());
+		
+		
+		// 생성된 몬스터 만큼 카운팅 됩니다.
+		mMonsterCount += 1;
+		
+		mMonster01[idx].idx = idx;
+		mMonster01[idx].RandomHP();//
+		monster.name = "Monster"+idx;
+		// 레이어 오더를 단계적으로 주어 몬스터들의 뎁스가 차례대로 겹치도록 한다.
+		monster.GetComponent<SpriteRenderer>().sortingOrder = idx+1;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// 버튼의 명령을 받았을때, 처리 하는 곳.
+	void OnButtonDown(string trigger){
+		if (trigger == "Back") {
+			//mCameraControl.SetStatus(CameraControl.Status.Start);
+			Invoke("StartButton",0.5f);
+		}
+
+
+		//캐릭터 상태 동작.
+		if (trigger == "ChangeStatus") {
+			mHero01.SetStatus(HeroControl.Status.Attack);
+			Debug.Log("Status Change good");
+
+			//mMonster01.SetStatus(MonsterControl.Status.Damaged);
+		}
+
+		if (trigger == "StopStatus") {
+			mHero01.SetStatus(HeroControl.Status.Idle);
+			Debug.Log("idle ok");
+
+			//mMonster01.SetStatus(MonsterControl.Status.UseSkill);
+
+		}
+		if (trigger == "UseSkill") {
+			mHero01.SetStatus(HeroControl.Status.UseSkill);
+			Debug.Log("UseSkill Change good");
+
+			//mMonster01.SetStatus(MonsterControl.Status.Dead);
+		}
+		
+		if (trigger == "Damaged") {
+			mHero01.SetStatus(HeroControl.Status.Damaged);
+			Debug.Log("Damaged ok");
+
+			//mMonster01.SetStatus(MonsterControl.Status.Attack);
+		}
+		if (trigger == "Dead") {
+			mHero01.SetStatus(HeroControl.Status.Dead);
+			Debug.Log("Dead ok");
+
+			//mMonster01.SetStatus(MonsterControl.Status.Idle);
+
+		}
+		
+	}
+	// 버튼의 명령 처리 함수 
+	void StartButton(){
+		Application.LoadLevel("Menu_Default_Scene");
+	}
+
+}
---
+++ Assets/Scrpts/MonsterControl.cs
@@ -0,0 +1,171 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterControl : MonoBehaviour {
+
+	//게임메니저에서 몬스터를 컨트롤 한다.
+	public In_GameManager mIn_GameManager;
+	
+	//mAnimator를 선언.
+	private Animator mAnimator;
+
+	//몬스터 숫자.
+	public int idx;
+	
+	//몬스터 의 공격력, 채력, 공격 속도에 사용 될 변수.
+	// HP
+	public int mOrinHP;
+	[HideInInspector]
+	public int mHP;
+	
+	//MANA
+	public int mOrinMP;
+	[HideInInspector]
+	public int mMP;
+	
+	//공격 데미지 (현제는 걍 데미지만.. 나중에 공격력이랑 수정 필요)
+	public int mOrinAttack;
+	[HideInInspector]
+	public int mAttack;
+	
+	//공속 
+	public float mAttackSpeed;
+	
+	//추후 스킬 구현..스킬 리스트.??
+	
+	//히어로 의 상태 (대기, 달림, 공격, 사망)
+	public enum Status
+	{
+		Idle,
+		Attack,
+		Damaged,
+		UseSkill,
+		Dead
+	}
+	
+	//public으로 선언 되었지만, 인스팩터 뷰에서 노출되지 않기를 원하는 경우 
+	//hideinspector를 선언합니다.
+	[HideInInspector]
+	public Status mStatus = Status.Idle; //히이로의 기본상태를 idle로 설정.
+	
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		//1.HP 넣고, 2. 백그라운드 컴퍼넌트 넣고, 3. 활이 발사될 장소를 넣고. 스타트
+		mHP = mOrinHP;
+		mAttack = mOrinAttack;
+		
+		//Archer의 Animator 컴포넌트 레퍼런스를 가져옵니다.
+		//이 script가 붙은 gameObject에 Animator를 가져옴.
+		mAnimator = gameObject.GetComponent<Animator> ();
+		
+		
+	}
+
+	//상테와 파라메터를 통해 아처의 상태를 컨트롤 합니다.
+	public void SetStatus(Status status)
+	{
+		//animator 에서 만든 상태 간 전이를 상황에 맞게 호출 한다.
+		switch (status) {
+		case Status.Idle:
+			mAnimator.SetTrigger("Idle");
+			Debug.Log("MM idle---");
+			break;
+			
+		case Status.Attack:
+			mAnimator.SetTrigger("Attack");
+			Debug.Log("MM Attack---");
+			break;
+			
+		case Status.Dead:
+			mAnimator.SetTrigger("Dead");
+			Debug.Log("MM Die---");
+			break;
+			
+		case Status.Damaged:
+			mAnimator.SetTrigger("Damaged");
+			Debug.Log("MM Damage---");
+			break;
+			
+		case Status.UseSkill:
+			mAnimator.SetTrigger("Skill");
+			Debug.Log("MMSkill---");
+			break;
+			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RandomHP()
+	{
+		mHP += Random.Range(-10, 10);    
+	}
+
+
+	public void Damaged()
+	{
+		Debug.Log ("monster hitted");
+		
+		GameObject Hero = GameObject.Find ("Hero");
+		HeroControl archercontrol = Hero.GetComponent<HeroControl> ();
+		
+		//mHP -= archercontrol.GetRandomDamage ();
+		mAnimator.SetTrigger ("Damage");
+		
+		
+		// 사망처리
+		if(mHP <= 0)
+		{
+			mStatus = Status.Dead;
+			mHP = 0;
+			//mCollider.enabled = false;
+			mAnimator.SetTrigger("Dead");
+			//mGameManager.ReAutoTarget();
+			Destroy(gameObject, 1f);
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
